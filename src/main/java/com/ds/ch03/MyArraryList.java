c1c2ae59d393d165f9d2de6ba777b369e4a2a886
dengqz <872114581@qq.com>
2017-10-06 22:21:10 +0800

    添加get、set方法

--- src/main/java/com/ds/ch03/MyArraryList.java
+++ src/main/java/com/ds/ch03/MyArraryList.java
@@ -24,12 +24,44 @@ public class MyArraryList<T> implements Iterable<T> {
 
     private void doClear(){
         theSize=0;
+        ensureCapacity(DEFAULT_CAPACITY);
     }
     public int size(){
         return theSize;
     }
     public boolean isEmpty(){
         return size()==0;
+    }
+
+    public void trimToSize()
+    {
+        ensureCapacity(size());
+    }
+
+    public T get(int idx)
+    {
+        if(idx < 0 || idx >= size())
+            throw new ArrayIndexOutOfBoundsException();
+        return theItems[idx];
+    }
+
+    public T set (int idx, T newVal)
+    {
+        if(idx < 0 || idx >= size())
+            throw new ArrayIndexOutOfBoundsException();
+        T old = theItems[idx];
+        theItems[idx] = newVal;
+        return old;
+    }
+    public void ensureCapacity(int newCapacity)
+    {
+        if(newCapacity < theSize)
+            return;
+
+        T [] old =theItems;
+        theItems = (T[]) new Object[ newCapacity];
+        for(int i = 0; i < size(); i++)
+            theItems[i] = old[i];
     }
     public Iterator<T> iterator() {
         return null;
