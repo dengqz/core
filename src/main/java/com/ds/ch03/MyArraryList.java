4f26711e240b5cc87b664395e1b465dd0db9b356
dengqz <872114581@qq.com>
2017-10-06 20:47:57 +0800

    first push

---
+++ src/main/java/com/ds/ch03/MyArraryList.java
@@ -0,0 +1,19 @@
+package com.ds.ch03;
+
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by carino on 2017/10/6.
+ */
+public class MyArraryList<T> implements Iterable<T> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private int theSize;
+    private T [] theItems;
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+}
